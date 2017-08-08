695e93bb42235c6b5b3667b3fcddab4d6800a2e3
Alexander Naumets <na.al@mail.com>
2017-08-08 15:20:25 +0300

    fix

--- backend/widgets/AdminMenu/views/main.php
+++ backend/widgets/AdminMenu/views/main.php
@@ -4,6 +4,7 @@ use yii\helpers\Html;
 use backend\widgets\AdminMenu\Asset;
 use common\modules\store\models\order\Orders;
 use common\modules\store\models\status\StatusRepository;
+use yii\helpers\Url;
 
 Asset::register($this);
 
@@ -13,12 +14,12 @@ Asset::register($this);
     <?php
     echo $widget->a('Главная','/');
     echo $widget->a('Текстовые страницы','/pages');
-    echo $widget->a('Banners','/ads-banners');
-    echo $widget->a('Таксономия','/taxonomy/vocabulary');
-    echo $widget->a('Тарифы (пакеты)','/packages');
-    echo $widget->a('Тарифы (опции)','/package-options');
-    echo $widget->a('Переводы','/language/source-message/index', ['active-url' => ['/language/source-message','/language/message','/language/default']]);
-    echo $widget->a('Пользователи','/user/admin');
+    echo $widget->a('Banners', Url::to(['/ads-banners']), ['active-url' => ['/ads-banners','/ads-regions']]);
+    echo $widget->a('Таксономия', Url::to(['/taxonomy/vocabulary']));
+    echo $widget->a('Тарифы (пакеты)',Url::to(['/packages']));
+    echo $widget->a('Тарифы (опции)',Url::to(['/package-options']));
+    echo $widget->a('Переводы',Url::to(['/language/source-message/index']), ['active-url' => ['/language/source-message','/language/message','/language/default']]);
+    echo $widget->a('Пользователи',Url::to(['/user/admin']));
     ?>
 </div>
 
