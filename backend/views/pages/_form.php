9e20e1720820c472efa9d5bd59b38184b7a681db
Alexander Naumets <na.al@mail.com>
2017-08-09 11:31:07 +0300

    Taxonomy fix

--- backend/views/pages/_form.php
+++ backend/views/pages/_form.php
@@ -1,8 +1,8 @@
 <?php
 
+use vova07\imperavi\Widget;
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
-use vova07\imperavi\Widget;
 
 /* @var $this yii\web\View */
 /* @var $model backend\models\Pages */
@@ -15,16 +15,18 @@ use vova07\imperavi\Widget;
 
     <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
 
+    <?= $form->field($model, 'test')->dropDownList([6319 => 'dwewer', 6318 => 'ewrwerw1sss'],[ 'prompt'=>'- Select category -']) ?>
+
     <?= $form->field($model, 'body')->widget(Widget::className(), [
-            'settings' => [
-                'lang' => 'ru',
-                'minHeight' => 600,
-                'plugins' => [
-                    'clips',
-                    'fullscreen'
-                ]
+        'settings' => [
+            'lang' => 'ru',
+            'minHeight' => 600,
+            'plugins' => [
+                'clips',
+                'fullscreen'
             ]
-        ]);
+        ]
+    ]);
     ?>
 
     <div class="form-group">
